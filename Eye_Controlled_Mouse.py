26041214f5ab2f399796d42a6380329f9c1aed4f
ObiWanPacino <luigipacino1@gmail.com>
2022-10-21 11:30:04 +0200

    Ho reso i landmark visibili

--- Eye_Controlled_Mouse.py
+++ Eye_Controlled_Mouse.py
@@ -7,6 +7,13 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = face_mesh.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
-    print(landmark_points)
+    frame_h, frame_w, _ = frame.shape
+    #print(landmark_points)
+    if landmark_points:
+        landmarks = landmark_points[0].landmark
+        for landmark in landmarks:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 0))
     cv2.imshow('ECM', frame)
     cv2.waitKey(1)
