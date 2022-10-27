44c32fdec14677cb060cf08c5576cd841e08b3fe
ObiWanPacino <luigipacino1@gmail.com>
2022-10-27 15:08:26 +0200

    Added left eye click command

--- Eye_Controlled_Mouse.py
+++ Eye_Controlled_Mouse.py
@@ -12,19 +12,23 @@ while True:
     output = face_mesh.process(rgb_frame)
     landmark_points = output.multi_face_landmarks
     frame_h, frame_w, _ = frame.shape
-    #print(landmark_points)
     if landmark_points:
         landmarks = landmark_points[0].landmark
-        #z = -1
         for id, landmark in enumerate(landmarks[474:478])  :
-            #z+=1
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 0))
-            #cv2.putText(frame,str(z),(x,y),80,0.3,(255,0,0),1)
             if id == 1:
                 screen_x = screen_w / frame_w * x
                 screen_y = screen_h / frame_h * y
                 pyautogui.moveTo(screen_x, screen_y)
+        left = [landmarks[145], landmarks[159]]
+        for landmark in left:
+            x = int(landmark.x * frame_w)
+            y = int(landmark.y * frame_h)
+            cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        if (left[0].y - left[1].y) < 0.010:
+            pyautogui.click()
+            pyautogui.sleep(1)
     cv2.imshow('ECM', frame)
     cv2.waitKey(1)
