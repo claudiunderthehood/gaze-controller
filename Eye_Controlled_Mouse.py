dfe85d6a7bffc1ed955160b719e3ef8267b37838
ObiWanPacino <luigipacino1@gmail.com>
2022-11-04 14:00:29 +0100

    Added map method

--- Eye_Controlled_Mouse.py
+++ Eye_Controlled_Mouse.py
@@ -2,7 +2,12 @@ import cv2
 import mediapipe as mp
 import pyautogui
 
+def map(a,b,c,d,x):
+    return(d*(x-a)+c*(b-x))/(b-a)
+
 cam = cv2.VideoCapture(0)
+cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1000)
+cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1000)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_w, screen_h = pyautogui.size() 
 while True: 
@@ -14,14 +19,21 @@ while True:
     frame_h, frame_w, _ = frame.shape
     if landmark_points:
         landmarks = landmark_points[0].landmark
-        for id, landmark in enumerate(landmarks[474:478])  :
+        right = [landmarks[473],landmarks[474],landmarks[475],landmarks[476],landmarks[477]]
+        center = [landmarks[386],landmarks[362]]
+        cx = center[0].x
+        cy = center[1].y
+        px = landmarks[473].x
+        py = landmarks[473].y
+        for id, landmark in enumerate(right)  :
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
-            cv2.circle(frame, (x, y), 3, (0, 255, 0))
-            if id == 1:
-                screen_x = screen_w / frame_w * x
-                screen_y = screen_h / frame_h * y
-                pyautogui.moveTo(screen_x, screen_y)
+            if id == 0:
+                screen_x = map(cx-0.003,cx+0.003,0,screen_w,px)
+                screen_y = map(cy-0.005,cy+0.005,0,screen_h,py)
+                pyautogui.moveTo(screen_x,screen_y)
+            else:
+                cv2.circle(frame, (x, y), 3, (0, 255, 0))
         left = [landmarks[145], landmarks[159]]
         for landmark in left:
             x = int(landmark.x * frame_w)
